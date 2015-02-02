3d1c222cf06333701deae7cb6e53e0da7a41490f
jorgeer <jfemail3@gmail.com>
2015-02-02 13:33:08 +0100

    config

--- dashvisor/backends/__init__.py
+++ dashvisor/backends/__init__.py
@@ -2,7 +2,8 @@ from django.conf import settings
 from django.utils.importlib import import_module
 
 def get_backend():
-    backend_path = getattr(settings, 'DASHVISOR_BACKEND', 'dashvisor.backends.file.Backend').split('.')
+    #backend_path = getattr(settings, 'DASHVISOR_BACKEND', 'dashvisor.backends.file.Backend').split('.')
+    backend_path = 'dashvisor.backends.file.Backend'.split('.')
     module_name = ".".join(backend_path[:-1])
     class_name = backend_path[-1]
     backend_module = import_module(module_name)
